fd9e879ba8c10ca7c063068f9f4488d2af396467
axlmrie <120368907+axlmrie@users.noreply.github.com>
2024-03-14 22:43:18 +0100

    edit

--- src/main/java/com/java/InfinityConnect/daos/VilainDao.java
+++ src/main/java/com/java/InfinityConnect/daos/VilainDao.java
@@ -1,6 +1,7 @@
 package com.java.InfinityConnect.daos;
 
 import com.java.InfinityConnect.entities.Vilain;
+import com.java.InfinityConnect.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VilainDao extends JpaRepository<Vilain, Long> {
